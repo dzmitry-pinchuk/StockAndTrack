9d5e4be7731a72908c3ef370fb78e3a7dd392cb1
Пинчук Дмитрий Григорьевич <pinchuk@adani.by>
2018-08-16 19:06:50 +0300

    update

--- src/main/java/com/qaprosoft/entity/transport/Track.java
+++ src/main/java/com/qaprosoft/entity/transport/Track.java
@@ -2,4 +2,17 @@ package com.qaprosoft.entity.transport;
 
 public class Track extends AbstractTransport{
 
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/qaprosoft/entity/transport/Train.java
+++ src/main/java/com/qaprosoft/entity/transport/Train.java
@@ -2,4 +2,16 @@ package com.qaprosoft.entity.transport;
 
 public class Train extends AbstractTransport{
 
+	private int numberOfWagons;
+
+	public int getNumberOfWagons() {
+		return numberOfWagons;
+	}
+
+	public void setNumberOfWagons(int numberOfWagons) {
+		this.numberOfWagons = numberOfWagons;
+	}
+	
+	
+	
 }

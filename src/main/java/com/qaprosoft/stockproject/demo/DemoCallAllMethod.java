87c3e5fcf25bd3f15f62e27ac581389b13aeacbd
Пинчук Дмитрий Григорьевич <pinchuk@adani.by>
2018-08-20 00:55:55 +0300

    update demo

--- src/main/java/com/qaprosoft/stockproject/demo/DemoCallAllMethod.java
+++ src/main/java/com/qaprosoft/stockproject/demo/DemoCallAllMethod.java
@@ -9,23 +9,27 @@ import org.apache.logging.log4j.Logger;
 
 import com.qaprosoft.stockproject.entity.Report;
 import com.qaprosoft.stockproject.entity.Stock;
+import com.qaprosoft.stockproject.entity.transport.Track;
+import com.qaprosoft.stockproject.entity.transport.Train;
 import com.qaprosoft.stockproject.parsing.ClassForJaxB;
 
 public class DemoCallAllMethod {
 	
 	private static final Logger logger = LogManager.getLogger();
 	private Scanner scan = new Scanner(System.in);
-	
 
 	public void start() {
 		
 		Report report = new Report();
-		
 		ClassForJaxB all = new ClassForJaxB();
-		logger.log(Level.INFO, "DataBase has Stocks: ");
 		ArrayList<Stock> stocks = all.getStocks();
+		ArrayList<Track> tracks = all.getTracks();
+		ArrayList<Train> trains = all.getTrains();	
+		
+		logger.log(Level.INFO, "DataBase has Stocks: ");
+		
 		logger.log(Level.INFO, stocks);
-		logger.log(Level.INFO, "Insert StockID first: ");
+		logger.log(Level.INFO, "Insert StockID first(1 - " + stocks.size() + "): ");
 		Long stockID1 = scan.nextLong();
 		for (int i = 0; i < stocks.size(); i++) {
 			if (stocks.get(i).getId() == stockID1) {
@@ -33,7 +37,7 @@ public class DemoCallAllMethod {
 			}
 		}
 
-		logger.log(Level.INFO, "Insert StockID second: ");
+		logger.log(Level.INFO, "Insert StockID second(1 - " + stocks.size() + "): ");
 		Long stockID2 = scan.nextLong();
 		for (int i = 0; i < stocks.size(); i++) {
 			if (stocks.get(i).getId() == stockID2) {
@@ -41,21 +45,36 @@ public class DemoCallAllMethod {
 			}
 		}
 		
+		if ( stockID1<0 || stockID1>stocks.size() ||  stockID2<0 || stockID2>stocks.size()) {
+			throw new UnsupportedOperationException("You must insert correct value. Try again");
+		}
+		
 		logger.log(Level.INFO, "Select type of transport(1 - auto, 2 - train): ");
 		int type = scan.nextInt();
+		if ( type < 1 || type > 2 ) {
+			throw new UnsupportedOperationException("You must insert correct value. Try again");
+		}
 		switch (type) {
 		case 1:
-			logger.log(Level.INFO, all.getTracks());
+			logger.log(Level.INFO, tracks);
+			logger.log(Level.INFO, "Insert TransportID(1 - " + tracks.size() + "): ");
+			int trackID = scan.nextInt();
+			for (int i = 0; i < tracks.size(); i++) {
+				if (tracks.get(i).getId() == trackID) {
+					report.setTransport(tracks.get(i));
+				}
+			}		
 			break;
 		case 2:
-			logger.log(Level.INFO, all.getTrains());
-			break;
-		default:
-			logger.log(Level.ERROR, "Error. Insert onli 1 or 2");
+			logger.log(Level.INFO, trains);
+			logger.log(Level.INFO, "Insert TransportID(1 - " + trains.size() + "): ");
+			int trainID = scan.nextInt();
+			for (int i = 0; i < trains.size(); i++) {
+				if (trains.get(i).getId() == trainID) {
+					report.setTransport(trains.get(i));
+				}
+			}		
 			break;
 		}
-		logger.log(Level.INFO, "Insert TransportID: ");
-		int transportID = scan.nextInt();
-		
 	}
 }
